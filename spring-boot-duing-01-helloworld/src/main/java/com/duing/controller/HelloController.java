d76a49347dbe869dac460ea49ebd6376dc1178c2
1801708121 <1801708121@qq.com>
2023-05-02 15:25:35 +0800

    spring-boot-duing-01-helloworld

---
+++ spring-boot-duing-01-helloworld/src/main/java/com/duing/controller/HelloController.java
@@ -0,0 +1,26 @@
+package com.duing.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Controller
+ * 控制类   业务逻辑  请求分发  组装响应
+ */
+@Controller
+public class HelloController {
+    /**
+     * @RequestMapping
+     * 指定方法和请求之间的映射关系
+     * @ResponseBody
+     * 代表返回的是json格式的数据
+     * @return
+     */
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "Hello Duing!";
+    }
+
+}
