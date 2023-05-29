404069397b08b352faf24a1fa8cc969f6c3f0768
1801708121 <1801708121@qq.com>
2023-05-29 16:28:48 +0800

    RESTful设计的使用

---
+++ spring-boot-duing-03-web-restful/src/main/java/com/duing/controller/GuestController.java
@@ -0,0 +1,71 @@
+package com.duing.controller;
+
+import com.duing.bean.Guest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import com.duing.service.GuestService;
+
+import java.util.List;
+
+/**
+ * 业务控制类  返回视图
+ * @RequestMapping("/guest")
+ * 放在类上面，代表类里面的方法是以此开头的
+ */
+@Controller
+@RequestMapping("/guest")
+public class GuestController {
+
+    @Autowired
+    private GuestService guestService;
+
+//    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
+    public String list(Model model){
+        List<Guest> guestList = guestService.list();
+        model.addAttribute("guestList",guestList);
+        return "list";
+    }
+
+    @GetMapping("/toAdd")
+    public String toAdd(){
+        return "add";
+    }
+
+    @PostMapping
+    public String add(Guest guest){
+        guestService.add(guest);
+        return "redirect:/guest";
+    }
+
+    /**
+     * 通过/guest/toUpdate/{name}格式的url映射到此方法
+     * 其中的name属性值通过注解@PathVariable映射得到此方法中
+     * 其中的name代表去url中查找的属性值
+     * 直译的意思是  从路径中获取name的值
+     * @param model
+     * @param name
+     * @return
+     */
+    @GetMapping("/toUpdate/{name}")
+    public String toUpdate(Model model,@PathVariable("name") String name){
+        Guest guest = guestService.get(name);
+        model.addAttribute("guest",guest);
+        return "update";
+    }
+
+    @PutMapping
+    public String update(Guest guest){
+        guestService.update(guest);
+        return "redirect:/guest";
+    }
+
+    @DeleteMapping("/{name}")
+    public String delete(@PathVariable("name") String name){
+        guestService.delete(name);
+        return "redirect:/guest";
+    }
+
+}
